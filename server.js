e99ff10d4bf662a0cc385e21dac0a500d68e0567
Abhijith P J <apj42211@gmail.com>
2026-02-20 20:53:33 +0530

    api added

--- server.js
+++ server.js
@@ -23,7 +23,10 @@ connectDB();
 
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], 
+    origin: [
+        'http://localhost:5173', 
+        process.env.FRONTEND_URL
+    ].filter(Boolean), 
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
 };
